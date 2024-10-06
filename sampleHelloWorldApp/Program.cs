1efa74bc9ad89478c6f44f4762ebd437e7f7280a
hariomgavhane14 <hariomgavhane14@gmail.com>
2024-10-06 14:36:37 +0530

    adding hello world files

---
+++ sampleHelloWorldApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sampleHelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
